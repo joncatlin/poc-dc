1790f8d10aa961f0c15b303b90cad5e6169b5d08
DESKTOP-CM4DN2T\joncatlin <jonc@destini.com>
2020-02-15 14:14:16 -0700

    Changed the SMS handler to accept form encoded data and not a json object

--- webhooks/src/main.rs
+++ webhooks/src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{post, App, Error, HttpResponse, HttpServer};
+use actix_web::{post, web, App, Error, HttpResponse, HttpServer, Result};
 use bytes::Bytes; 
 
 #[macro_use]
@@ -10,6 +10,7 @@ use std::io::Write;
 use chrono::Local;
 use env_logger::Builder;
 use log::LevelFilter;
+use serde::{Deserialize, Serialize};
 
 // Body of the response when the msg processed successfully
 const OK_STATUS: &str = "{ \"status\" : \"Ok\" }";
@@ -37,33 +38,59 @@ async fn main() -> std::io::Result<()> {
             .service(sms_hook)
             .service(email_hook)
         })
-        .bind("127.0.0.1:8088")?
+        .bind("0.0.0.0:80")?
+//        .bind("127.0.0.1:80")?
         .run()
         .await
 }
 
 
-#[post("/csc/webhooks/sms")]
-async fn sms_hook(body: Bytes) -> Result<HttpResponse, Error> {
-    // body is loaded, now we can deserialize json-rust
-    let result = json::parse(std::str::from_utf8(&body).unwrap()); // return Result
-    match result {
-        Ok(v) => { 
-            info!("EVENT-{}", v.dump());
-            return Ok(HttpResponse::Ok()
-                .content_type("application/json")
-                .body(OK_STATUS));
-        },
-        Err(e) => {
-            warn!("EVENT-body contained {} and parse error was: {}", std::str::from_utf8(&body).unwrap(), e.to_string());
-            let err_msg = json::object! {"err" => e.to_string()};
-            return Ok(HttpResponse::BadRequest()
-                .content_type("application/json")
-                .body(err_msg.dump()
-            ));
-        }
-    };
+
+#[derive(Serialize, Deserialize)]
+struct SMSData {
+    SmsSid: String,
+    SmsStatus: String,
 }
+
+
+// var smsSid = Request.Form["SmsSid"];
+// var messageStatus = Request.Form["MessageStatus"];
+#[post("/csc/webhooks/sms")]
+async fn sms_hook(form: web::Form<SMSData>) -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok()
+        .content_type("text/plain")
+        .body(format!("Welcome smsSid={} ans messageStatus={}!", form.SmsSid, form.SmsStatus)))
+}
+
+
+
+// async fn sms_hook(form: web::Form<FormData>) -> Result<String> {
+
+//     Ok(format!("Welcome smsSid={} ans messageStatus={}!", form.smsSid, form.messageStatus))
+// }
+
+
+// #[post("/csc/webhooks/sms")]
+// async fn sms_hook(body: Bytes) -> Result<HttpResponse, Error> {
+//     // body is loaded, now we can deserialize json-rust
+//     let result = json::parse(std::str::from_utf8(&body).unwrap()); // return Result
+//     match result {
+//         Ok(v) => { 
+//             info!("EVENT-{}", v.dump());
+//             return Ok(HttpResponse::Ok()
+//                 .content_type("application/json")
+//                 .body(OK_STATUS));
+//         },
+//         Err(e) => {
+//             warn!("EVENT-body contained {} and parse error was: {}", std::str::from_utf8(&body).unwrap(), e.to_string());
+//             let err_msg = json::object! {"err" => e.to_string()};
+//             return Ok(HttpResponse::BadRequest()
+//                 .content_type("application/json")
+//                 .body(err_msg.dump()
+//             ));
+//         }
+//     };
+// }
 
 
 #[post("/csc/webhooks/email")]
