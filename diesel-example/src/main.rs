a3076c136f76f53630b22355e738b9d8f753d767
Jon Catlin <jonc@destini.com>
2020-03-23 03:28:27 -0700

    Commented out unused  use statement

--- diesel-example/src/main.rs
+++ diesel-example/src/main.rs
@@ -7,7 +7,7 @@
 extern crate diesel;
 
 use actix_web::{get, middleware, post, web, App, Error, HttpResponse, HttpServer};
-use diesel::prelude::*;
+//use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
 use diesel::pg::PgConnection;
 use uuid::Uuid;
